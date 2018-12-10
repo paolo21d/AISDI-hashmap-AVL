6c47c4f53cfa11094ddec898f07f1601317ff065
paolo21d <paolo21d@gmail.com>
2018-12-10 23:36:34 +0100

    Initial commit - Debug & Release

---
+++ Release/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/paul/Pulpit/AISDI-lab/Mapy
+# Build directory: /home/paul/Pulpit/AISDI-lab/Mapy/Release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
---
+++ Release/tests/CMakeFiles/aisdiMapsTests.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/paul/Pulpit/AISDI-lab/Mapy/tests/HashMapTests.cpp" "/home/paul/Pulpit/AISDI-lab/Mapy/Release/tests/CMakeFiles/aisdiMapsTests.dir/HashMapTests.cpp.o"
+  "/home/paul/Pulpit/AISDI-lab/Mapy/tests/TreeMapTests.cpp" "/home/paul/Pulpit/AISDI-lab/Mapy/Release/tests/CMakeFiles/aisdiMapsTests.dir/TreeMapTests.cpp.o"
+  "/home/paul/Pulpit/AISDI-lab/Mapy/tests/test_main.cpp" "/home/paul/Pulpit/AISDI-lab/Mapy/Release/tests/CMakeFiles/aisdiMapsTests.dir/test_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
